5a930816fe3fa45aaa244f5249526a95b0e7a0c1
NukeTurtle <stiglic.elvis@gmail.com>
2021-09-09 15:06:26 +0300

    First iteration of the project with all filesand folders. Needs to be tested and design might still change.

---
+++ functions.js
@@ -0,0 +1,9 @@
+const app=document.querySelector("gallery");
+const apiKey="Vi2su_15UigI-fG4W1JdJQDoqCKmXnVfK22AX5KZEKo";
+const request=new XMLHttpRequest;
+request.open("GET","https://api.unsplash.com/photos/?client_id=" + apiKey,!0);
+request.onload=function(){JSON.parse(this.response).forEach(e=>{const t=document.createElement("div");
+t.setAttribute("class","card");
+const n=document.createElement("img");
+n.src=e.urls.small;
+const s=document.createElement("a"),a=document.createElement("h3");a.textContent=e.user.first_name,s.appendChild(a),s.href=e.links.html,gallery.appendChild(t),t.appendChild(n),document.createElement("br"),t.appendChild(s)})},request.send();
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var {src,dest} = require("gulp");
+var minifyJs = require("gulp-uglify");
+var concat = require("gulp-concat");
+var minifyCss = require("gulp-clean-css");
+
+gulp.task("default", function() {
+    return (src("*.css")
+    /* we merge all css files into one */
+    .pipe(concat("style.css"))
+    .pipe(dest("./prod/css/"))
+    /* we minify/uglify the css file for production environment */
+    .pipe(minifyCss("./prod/css/style.css")),
+    src("functions.js")
+    /* we minify/uglify the js file for production environment */
+    .pipe(minifyJs())
+    .pipe(gulp.dest("./prod/js/")));
+});
