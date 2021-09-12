b17680d5fc7f85daf4c679126feba33f343067c6
Elvis Stiglic <41340032+NukeTurtle@users.noreply.github.com>
2021-09-13 00:58:47 +0300

    Fixed gulpfile to now minify CSS correctly

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,18 @@ var concat = require("gulp-concat");
 var minifyCss = require("gulp-clean-css");
 
 gulp.task("default", function() {
-    return (src("*.css")
+    return (
     /* we merge all css files into one */
+    src("*.css")
     .pipe(concat("style.css"))
     .pipe(dest("./prod/css/"))
     
     /* we minify/uglify the css file for production environment */
-    .pipe(minifyCss("./prod/css/style.css")),
-    src("functions.js")
+    .pipe(minifyCss("prod/css/style.css"))
+    .pipe(dest("./prod/css/")),
     
     /* we minify/uglify the js file for production environment */
+    src("functions.js")
     .pipe(minifyJs())
-    .pipe(gulp.dest("./prod/js/")));
+    .pipe(dest("./prod/js/")));
 });
